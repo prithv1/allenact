cf85079f7ea49f19f0f33bbce2e57dd7f8c0f9de
Prithvijit Chattopadhyay <prithvijitc@vision-server3>
2020-10-03 01:03:47 -0700

    Stupid importlib error fixed

--- pointnav_aws_jobs/run_baseline_p3_8x.sh
+++ pointnav_aws_jobs/run_baseline_p3_8x.sh
@@ -3,5 +3,5 @@
 
 python main.py \
     -o storage/robothor-pointnav-rgb-resnet \
-    -b projects/tutorials/pointnav_aws_configs pointnav_robothor_general_rgb_ddppo_p3_8x_large.py \
+    -b projects/tutorials/pointnav_aws_configs pointnav_robothor_general_rgb_ddppo_p3_8x_large \
     -et v1_vanilla_ec2_p3_8x_large
