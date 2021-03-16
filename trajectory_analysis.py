4db315d7a5a5af1b5f8caa054dd1dc653003f5a4
Prithvijit Chattopadhyay <prithvijitc@vision-server3>
2021-03-15 18:59:23 -0700

    Analysis code

--- trajectory_analysis.py
+++ trajectory_analysis.py
@@ -128,9 +128,8 @@ def stop_fail_pos(data_df):
     when the goal is not in range
     """
     # Try a global approach
-    sub_df = data_df[
-        ["setting", "success", "took_end_action", "taken_actions", "goal_in_range"]
-    ]
+    sub_df = data_df[[data_df["took_end_action"] == True]]
+    sub_df = sub_df[["setting", "success", "taken_actions", "goal_in_range"]]
     sub_df["stop_fail_pos"] = sub_df.apply(lambda x: goal_out_range_took_end(x), axis=1)
     stop_fail_pos_df = (
         sub_df.groupby(["setting"], as_index=False)["stop_fail_pos"].mean().round(2)
